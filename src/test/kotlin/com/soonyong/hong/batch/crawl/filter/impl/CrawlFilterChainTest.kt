6d4009f3c424e2f0d358f3bbd4a5d856d937160f
e406hsy <e406hsy@gmail.com>
2022-06-08 07:39:17 +0900

    chores: 테스트 명 오타 수정

--- src/test/kotlin/com/soonyong/hong/batch/crawl/filter/impl/CrawlFilterChainTest.kt
+++ src/test/kotlin/com/soonyong/hong/batch/crawl/filter/impl/CrawlFilterChainTest.kt
@@ -10,7 +10,7 @@ class CrawlFilterChainTest : AnnotationSpec() {
     private val falseFilter = CrawlFilter { false }
 
     @Test
-    fun `AND test (true, ture) then true`() {
+    fun `AND test (true, true) then true`() {
         val crawlFilterChain = CrawlFilterChain(
             delegate = trueFilter, delegateCondition = CrawlFilterChain.DelegateCondition.AND, next = trueFilter
         )
@@ -26,7 +26,7 @@ class CrawlFilterChainTest : AnnotationSpec() {
     }
 
     @Test
-    fun `AND test (false, ture) then false`() {
+    fun `AND test (false, true) then false`() {
         val crawlFilterChain = CrawlFilterChain(
             delegate = falseFilter, delegateCondition = CrawlFilterChain.DelegateCondition.AND, next = trueFilter
         )
@@ -50,7 +50,7 @@ class CrawlFilterChainTest : AnnotationSpec() {
     }
 
     @Test
-    fun `OR test (false, ture) then true`() {
+    fun `OR test (false, true) then true`() {
         val crawlFilterChain = CrawlFilterChain(
             delegate = falseFilter, delegateCondition = CrawlFilterChain.DelegateCondition.OR, next = trueFilter
         )
@@ -66,7 +66,7 @@ class CrawlFilterChainTest : AnnotationSpec() {
     }
 
     @Test
-    fun `OR test (true, ture) then true`() {
+    fun `OR test (true, true) then true`() {
         val crawlFilterChain = CrawlFilterChain(
             delegate = trueFilter, delegateCondition = CrawlFilterChain.DelegateCondition.OR, next = trueFilter
         )
